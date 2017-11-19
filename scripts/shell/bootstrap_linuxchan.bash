776048f8f7ccf5bd5321eb307ec3bba797e12b58
Alexey Veselov <ukiroot@gmail.com>
2017-11-19 23:05:24 +0300

    bootstrap_linuxchan.bash: script for bootstrap linuxchan was added

---
+++ scripts/shell/bootstrap_linuxchan.bash
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+set -o xtrace
+set -o verbose
+set -o errexit
+
+
+DIR_FOR_IMAGE="/var/lib/libvirt/images/min_dist/"
+IMAGE_NAME="linuxchan.img"
+IMAGE="${DIR_FOR_IMAGE}${IMAGE_NAME}"
+DISK_DEV="/dev/loop0"
+DISK_DEV_SECTION="p1"
+DISK_DEV_1="${DISK_DEV}${DISK_DEV_SECTION}"
+DEBIAN_VER="stretch"
+DIR_CHROOT="/mnt/debian"
+
+
+apt-get install -y \
+    debootstrap \
+    qemu-utils
+
+
+mkdir -p "${DIR_FOR_IMAGE}"
+cd "${DIR_FOR_IMAGE}"
+rm -f "${IMAGE}"
+qemu-img create "${IMAGE}" 1G
+rmmod loop || true
+modprobe loop max_part=15
+losetup "${DISK_DEV}" "${IMAGE}"
+
+fdisk "${DISK_DEV}" << "EOF"
+n
+p
+1
+
+
+a
+w
+EOF
+
+
+partprobe "${DISK_DEV}"
+sleep 5
+mkfs.ext4 "${DISK_DEV_1}"
+mkdir -p "${DIR_CHROOT}"
+mount -v "${DISK_DEV_1}" "${DIR_CHROOT}"
+
+
+debootstrap --verbose --include=sudo,locales,nano,wget,grub-pc --arch amd64 "${DEBIAN_VER}" "${DIR_CHROOT}" http://ftp.ru.debian.org/debian/
+
+
+cat > "${DIR_CHROOT}/etc/fstab" << "EOF"
+/dev/sda1       /               ext4        defaults        0       1
+EOF
+
+
+cat > "${DIR_CHROOT}/etc/apt/sources.list" << EOF
+deb http://ftp.ru.debian.org/debian ${DEBIAN_VER} main contrib non-free
+deb-src http://ftp.ru.debian.org/debian ${DEBIAN_VER} main contrib non-free
+
+deb http://ftp.debian.org/debian/ ${DEBIAN_VER}-updates main contrib non-free
+deb-src http://ftp.debian.org/debian/ ${DEBIAN_VER}-updates main contrib non-free
+
+deb http://ftp.debian.org/debian/ ${DEBIAN_VER}-backports main contrib non-free
+deb-src http://ftp.debian.org/debian/ ${DEBIAN_VER}-backports main contrib non-free
+
+deb http://ftp.ru.debian.org/debian-security ${DEBIAN_VER}/updates main contrib non-free
+deb-src http://ftp.ru.debian.org/debian-security ${DEBIAN_VER}/updates main contrib non-free
+EOF
+
+cat > "${DIR_CHROOT}/root/postinst.sh" << EOF
+#!/bin/bash
+
+apt-get update
+
+chmod +x /etc/rc.local
+
+tzselect << "OEF"
+8
+39
+2
+1
+OEF
+TZ='Europe/Moscow'; export TZ
+
+useradd -m -s /bin/bash tester
+passwd tester << "OEF"
+tester
+tester
+OEF
+
+passwd << "OEF"
+admin
+admin
+OEF
+
+cat > /etc/default/locale << OEF
+LANG=en_US.UTF-8
+OEF
+
+cat > /etc/locale.gen << OEF
+en_US.UTF-8 UTF-8
+OEF
+
+locale-gen
+
+apt-get -y install linux-image-amd64 firmware-linux firmware-ralink firmware-realtek firmware-atheros ssh
+apt-get clean
+
+sed -i 's/^#GRUB_TERMINAL.*/GRUB_TERMINAL="serial console"/' /etc/default/grub
+sed -i 's/^GRUB_CMDLINE_LINUX.*/GRUB_CMDLINE_LINUX="console=ttyS0"/' /etc/default/grub
+
+
+grub-install "${DISK_DEV}" --modules="biosdisk part_msdos"
+sed -i 's/\/dev\/'"${DISK_DEV_1}"'/\/dev\/sda1/g' /boot/grub/grub.cfg
+sync
+EOF
+
+
+
+mount -v --bind /dev "${DIR_CHROOT}/dev"
+mount -vt devpts devpts "${DIR_CHROOT}/dev/pts"
+mount -vt proc proc "${DIR_CHROOT}/proc"
+mount -vt sysfs sysfs "${DIR_CHROOT}/sys"
+mount -vt tmpfs tmpfs "${DIR_CHROOT}/run"
+
+
+chroot "${DIR_CHROOT}" /bin/bash /root/postinst.sh
+chroot "${DIR_CHROOT}" /bin/bash -c "rm -fv /root/postinst.sh"
+
+umount -v "${DIR_CHROOT}/dev/pts"
+umount -v "${DIR_CHROOT}/dev"
+umount -v "${DIR_CHROOT}/proc"
+umount -v "${DIR_CHROOT}/sys"
+umount -v "${DIR_CHROOT}/run"
+
+
+umount -v "${DIR_CHROOT}"
+losetup -d "${DISK_DEV}"
--- scripts/shell/init_system.bash
+++ scripts/shell/init_system.bash
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 #Initializations of varibles
+DIR_MAIN=$(dirname `readlink -e "$0"`)
 VM_ISO="/var/lib/libvirt/images/VyOS.iso"
 VYOS_URL="https://downloads.vyos.io/release/1.1.8/vyos-1.1.8-amd64.iso"
 #Max value for NUMBER_OF_EUD is 9
 NUMBER_OF_EUD="8"
 EUT_BRIDGE="eud_bridge"
 BRIDGE_RESOURCE_MASK="mighty_bridge_"
+LINUXCHCHAN=("ursamajor" \
+"ursaminor" \
+"lupus" \
+"pisces" \
+"vulpecula" \
+"aquarius" \
+"aries" \
+"pisces"
+)
+
 
 #Initializations of functions
 function install_python_packages_via_apt() (
@@ -169,6 +180,167 @@ function create_eut_config() (
 EOF
 )
 
+
+function linuxchan_hostname_hack() (
+   DIR_FOR_IMAGE="/var/lib/libvirt/images/min_dist/"
+   IMAGE_NAME="linuxchan.img"
+   IMAGE="${DIR_FOR_IMAGE}${IMAGE_NAME}"
+   DISK_DEV="/dev/loop0"
+   DISK_DEV_SECTION="p1"
+   DISK_DEV_1="${DISK_DEV}${DISK_DEV_SECTION}"
+   DIR_CHROOT="/mnt/debian"
+
+
+   losetup "${DISK_DEV}" "${IMAGE}"
+   mount -v "${DISK_DEV_1}" "${DIR_CHROOT}"
+   mount -v --bind /dev "${DIR_CHROOT}/dev"
+   mount -vt devpts devpts "${DIR_CHROOT}/dev/pts"
+   mount -vt proc proc "${DIR_CHROOT}/proc"
+   mount -vt sysfs sysfs "${DIR_CHROOT}/sys"
+   mount -vt tmpfs tmpfs "${DIR_CHROOT}/run"
+
+
+   cat > "${DIR_CHROOT}/etc/rc.local" << "EOF" 
+#!/bin/sh -e
+HOSTNAME=`cat /proc/cmdline | grep -oE 'hostname=[a-z0-9\\-]+' | sed 's/hostname=//'`
+if [ "${HOSTNAME}" != '' ]; then
+    /bin/hostname "${HOSTNAME}"
+fi
+exit 0
+EOF
+
+   cat > "${DIR_CHROOT}/root/grub_cutomize.sh" << EOF
+LINUXCHCHAN=${LINUXCHCHAN}
+EOF
+
+   cat > "${DIR_CHROOT}/root/grub_cutomize.sh" << "EOF"
+seq 8 | while read VM_ID; do
+   HOSTNAME=${LINUXCHCHAN[$(( ${VM_ID} - 1 ))]}
+   cat /boot/grub/grub.cfg | grep -A20 "menuentry 'Debian GNU/Linux'" | grep -B 20 '^}' | \
+   sed -e 's/Debian GNU\/Linux/'${HOSTNAME}'/' -e 's/console=ttyS0/console=ttyS0 hostname='${HOSTNAME}'/' >> /etc/grub.d/40_custom
+done
+update-grub2
+EOF
+
+
+   chroot "${DIR_CHROOT}" /bin/bash /root/grub_cutomize.sh
+   chroot "${DIR_CHROOT}" /bin/bash -c "rm -fv /root/grub_cutomize.sh"
+
+   umount -v "${DIR_CHROOT}/dev/pts"
+   umount -v "${DIR_CHROOT}/dev"
+   umount -v "${DIR_CHROOT}/proc"
+   umount -v "${DIR_CHROOT}/sys"
+   umount -v "${DIR_CHROOT}/run"
+   umount -v "${DIR_CHROOT}"
+   losetup -d "${DISK_DEV}"
+)
+
+
+function create_linuxchan_config() (
+   MAC_1="52:54:00:9e:77:${VM_ID}7"
+   MAC_2="52:54:00:c5:77:${VM_ID}8"
+   CONSOLE_PORT="770${VM_ID}"
+
+   virsh list --all | grep " ${VM_NAME} " | while read NAME; do
+   if [ "`echo ${NAME} | grep running | awk '{print $2}'`" = "${VM_NAME}" ]; then
+       virsh destroy "${VM_NAME}"
+   fi
+   virsh undefine "${VM_NAME}"
+   done
+
+   cat > /etc/libvirt/qemu/${VM_NAME}.xml << EOF
+<domain type='kvm'>
+    <name>${VM_NAME}</name>
+    <uuid>14a0a55f-83b9-4917-7777-7${VM_ID}7${VM_ID}7${VM_ID}7${VM_ID}7${VM_ID}7${VM_ID}</uuid>
+    <memory unit='KiB'>524288</memory>
+    <currentMemory unit='KiB'>524288</currentMemory>
+    <vcpu placement='static'>1</vcpu>
+    <os>
+        <type arch='x86_64' machine='pc-i440fx-2.8'>hvm</type>
+    </os>
+    <features>
+        <acpi/>
+        <apic/>
+        <vmport state='off'/>
+    </features>
+    <cpu mode='custom' match='exact'>
+        <model fallback='allow'>kvm64</model>
+    </cpu>
+    <clock offset='utc'>
+        <timer name='rtc' tickpolicy='catchup'/>
+        <timer name='pit' tickpolicy='delay'/>
+        <timer name='hpet' present='no'/>
+    </clock>
+    <on_poweroff>destroy</on_poweroff>
+    <on_reboot>restart</on_reboot>
+    <on_crash>restart</on_crash>
+    <pm>
+        <suspend-to-mem enabled='no'/>
+        <suspend-to-disk enabled='no'/>
+    </pm>
+    <devices>
+        <emulator>/usr/bin/kvm</emulator>
+        <disk type='file' device='disk'>
+          <driver name='qemu' type='raw'/>
+          <source file='/var/lib/libvirt/images/min_dist/linuxchan.img'/>
+          <target dev='hda' bus='ide'/>
+          <boot order='1'/>
+          <address type='drive' controller='0' bus='0' target='0' unit='0'/>
+        </disk>
+        <controller type='usb' index='0' model='ich9-ehci1'>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x7'/>
+        </controller>
+        <controller type='usb' index='0' model='ich9-uhci1'>
+          <master startport='0'/>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x0' multifunction='on'/>
+        </controller>
+        <controller type='usb' index='0' model='ich9-uhci2'>
+          <master startport='2'/>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x1'/>
+        </controller>
+        <controller type='usb' index='0' model='ich9-uhci3'>
+          <master startport='4'/>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x2'/>
+        </controller>
+        <controller type='pci' index='0' model='pci-root'/>
+        <controller type='ide' index='0'>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x01' function='0x1'/>
+        </controller>
+        <controller type='virtio-serial' index='0'>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x0'/>
+        </controller>
+        <interface type='bridge'>
+          <mac address='${MAC_1}'/>
+          <source bridge='${EUT_BRIDGE}'/>
+          <model type='rtl8139'/>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+        </interface>
+        <interface type='bridge'>
+          <mac address='${MAC_2}'/>
+          <source bridge='${EUT_BRIDGE}'/>
+          <model type='rtl8139'/>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+        </interface>
+        <serial type='tcp'>
+          <source mode='bind' host='127.0.0.1' service='${CONSOLE_PORT}'/>
+          <protocol type='telnet'/>
+          <target port='0'/>
+        </serial>
+        <console type='tcp'>
+          <source mode='bind' host='127.0.0.1' service='${CONSOLE_PORT}'/>
+          <protocol type='telnet'/>
+          <target type='serial' port='0'/>
+        </console>
+        <input type='mouse' bus='ps2'/>
+        <input type='keyboard' bus='ps2'/>
+        <memballoon model='virtio'>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x0'/>
+        </memballoon>
+    </devices>
+</domain>
+EOF
+)
+
 function get_vyos() (
     wget -O "${VM_ISO}" "${VYOS_URL}"
 )
@@ -183,6 +355,14 @@ seq "${NUMBER_OF_EUD}" | while read VM_ID; do
    create_eut_config
 done
 
+bash "${DIR_MAIN}/bootstrap_linuxchan.bash"
+linuxchan_hostname_hack
+seq "${NUMBER_OF_EUD}" | while read VM_ID; do
+   VM_NAME=${LINUXCHCHAN[$(( ${VM_ID} - 1 ))]}
+   create_linuxchan_config
+done
+
 init_bridge_interfaces
 
-systemctl restart libvirtd
+#systemctl restart libvirtd
+systemctl reload libvirtd
