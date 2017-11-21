610b253c3646bc2e8af65b0fed92ec3acfed3dd4
Alexey Veselov <ukiroot@gmail.com>
2017-11-22 00:53:23 +0300

    euts.py: added functions for lock and unlock resources from pool

--- lib/euts.py
+++ lib/euts.py
@@ -156,8 +156,56 @@ def add_resource(uri, body):
     resapi_request('POST', uri, body, {"Content-Type": "application/json"})
 
 
+def update_resource(uri, body):
+    resapi_request('PUT', uri, body, {"Content-Type": "application/json"})
+
+
+def get_resource(uri):
+    response = resapi_request('GET', uri)
+    return response
+
+
 def add_eut(body):
     add_resource(pool_url_eut, body)
+
+
+def get_eut():
+    return lock_resource(pool_url_eut, 'eut')
+
+
+def get_bridge():
+    return lock_resource(pool_url_bridge, 'bridge')
+
+
+def get_linuxchan():
+    return lock_resource(pool_url_linuxchan, 'linuxchan')
+
+
+def lock_resource(url, resource_type):
+    response = get_resource(url)
+    for resource in json.loads(response.text)[resource_type]:
+        if resource['available'] is True:
+            resource['available'] = False
+            url_intance = pool_url_eut + slash_char + str(resource['id'])
+            update_resource(url_intance, resource)
+            return resource
+
+
+def return_eut(body):
+    return_resource(pool_url_eut, url)
+
+
+def return_bridge(body):
+    return_resource(pool_url_bridge, url)
+
+
+def return_linuxchan(body):
+    return_resource(pool_url_linuxchan, url)
+
+
+def return_resource(body, url):
+    body['available'] = True
+    update_resource(url + slash_char + str(body['id']), body)
 
 
 def add_bridge(body):
