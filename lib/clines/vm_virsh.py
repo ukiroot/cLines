f8f1ea6a2ada37c61a743537153387d8fad0a3be
Alexey Veselov <ukiroot@gmail.com>
2017-12-17 11:37:26 +0300

    vm_virsh.py: module was added for virsh shell commands

---
+++ lib/clines/vm_virsh.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+import re
+
+
+def start_vm(vm):
+    os.system('sudo virsh start ' + vm)
+
+
+def destroy_vm(vm):
+    os.system('sudo virsh destroy ' + vm)
+
+
+def shutdown_vm(vm):
+    os.system('sudo virsh shutdown ' + vm)
+
+
+def init_bridge_interface(bridge):
+    os.system('sudo ip link add name ' + bridge + ' type bridge')
+    os.system('sudo ip link set dev ' + bridge + ' up')
+
+
+def get_vm_interface(vm):
+    return re.findall(
+        ('vnet[0-9]+'),
+        subprocess.check_output(
+            'sudo virsh domiflist ' +
+            vm +
+            "| grep vnet | awk '{print $1}'",
+            shell=True
+        ).decode("utf-8", "strict")
+    )
